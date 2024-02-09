4e18b1c2dfa81d2c011ee4e5d5a658eb85330f72
DBKynd <db@dbkynd.com>
2024-02-09 01:50:14 -0700

    add vuetify

--- equipment/_build/src/App.vue
+++ equipment/_build/src/App.vue
@@ -1,9 +1,42 @@
 <template>
-  <main>
-    <h1>Hello World</h1>
-  </main>
+  <v-app>
+    <v-main>
+      <v-tabs v-model="tab" bg-color="primary" fixed-tabs>
+        <v-tab value="hardware" prepend-icon="mdi-sitemap">Hardware Chart</v-tab>
+        <v-tab value="audio" prepend-icon="mdi-audio-input-stereo-minijack">Audio Chart</v-tab>
+        <v-tab value="gamingpc" prepend-icon="mdi-controller">Gaming PC</v-tab>
+        <v-tab value="streamingpc" prepend-icon="mdi-application-braces">Streaming PC</v-tab>
+      </v-tabs>
+
+      <v-window v-model="tab">
+        <v-window-item value="hardware">
+          <HardwareTab />
+        </v-window-item>
+
+        <v-window-item value="audio">
+          <AudioTab />
+        </v-window-item>
+
+        <v-window-item value="gamingpc">
+          <GamingPcTab />
+        </v-window-item>
+
+        <v-window-item value="streamingpc">
+          <StreamingPcTab />
+        </v-window-item>
+      </v-window>
+    </v-main>
+  </v-app>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import AudioTab from '@/components/AudioTab.vue'
+import GamingPcTab from '@/components/GamingPcTab.vue'
+import HardwareTab from '@/components/HardwareTab.vue'
+import StreamingPcTab from '@/components/StreamingPcTab.vue'
+
+const tab = ref('streamingpc')
+</script>
 
 <style scoped></style>
---
+++ equipment/_build/src/components/StreamingPcTab.vue
@@ -0,0 +1,5 @@
+<template>StreamingPC</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
