4dd24791cdd598e66fffd9be9ffba0cf030120e7
DBKynd <dave@dbkynd.com>
2024-02-16 15:52:39 -0700

    remove item svg

--- equipment/_build/src/components/PcEquipment.vue
+++ equipment/_build/src/components/PcEquipment.vue
@@ -6,12 +6,11 @@
           class="item-list pa-1"
           v-for="(item, index) in items"
           :key="item.name"
-          @click="selectItem(item)"
-          :class="{ selected: selectedItem && item.name == selectedItem.name }"
+          @click="selectedIndex = index"
+          :class="{ selected: index == selectedIndex }"
         >
           <v-icon :icon="`mdi-${item.icon}`"></v-icon>
           {{ item.name }}
-          <ItemArrowSvg class="svg" :line="lines[index]" />
         </div>
       </v-col>
       <v-col cols="6">
@@ -20,11 +19,11 @@
           <v-btn
             density="compact"
             icon="mdi-circle-medium"
-            v-for="item in items"
+            v-for="(item, index) in items"
             :key="item.name"
-            @click="selectItem(item)"
+            @click="selectedIndex = index"
             class="item-button"
-            :class="{ selected: selectedItem && item.name == selectedItem.name }"
+            :class="{ selected: index == selectedIndex }"
             :style="`top: ${item.top}%; left: ${item.left}%;`"
             size="small"
           />
@@ -38,8 +37,7 @@
 </template>
 
 <script setup lang="ts">
-import ItemArrowSvg from '@/components/ItemArrowSvg.vue';
-import { computed, ref, onMounted, onBeforeUnmount } from 'vue';
+import { computed, ref } from 'vue';
 import ItemCard from '@/components/ItemCard.vue';
 
 const props = defineProps<{
@@ -58,40 +56,9 @@ const items = computed<Item[]>(() => {
   return array;
 });
 
-const selectedItem = ref<Item>();
-function selectItem(item: Item) {
-  selectedItem.value = item;
-}
-
-const lines = ref<Line[]>([]);
-function getLines() {
-  const listElements = document.querySelectorAll('.item-list');
-  const buttonElements = document.querySelectorAll('.item-button');
-  const heightOffset = 48;
-  lines.value = [];
-  for (let i = 0; i < items.value.length; i++) {
-    const itemRect = listElements[i].getBoundingClientRect();
-    const buttonRect = buttonElements[i].getBoundingClientRect();
-    const points: Point[] = [];
-    points.push({ x: itemRect.right, y: itemRect.top - heightOffset });
-    points.push({ x: itemRect.right, y: itemRect.bottom - heightOffset });
-    points.push({
-      x: buttonRect.left + buttonRect.width / 2,
-      y: buttonRect.top + buttonRect.height / 2 - heightOffset,
-    });
-    lines.value.push({ points });
-  }
-}
-
-onMounted(() => {
-  getLines();
-  window.addEventListener('load', getLines);
-  window.addEventListener('resize', getLines);
-});
-
-onBeforeUnmount(() => {
-  window.removeEventListener('load', getLines);
-  window.removeEventListener('resize', getLines);
+const selectedIndex = ref<number>(-1);
+const selectedItem = computed(() => {
+  return items.value[selectedIndex.value];
 });
 </script>
 
@@ -122,22 +89,15 @@ onBeforeUnmount(() => {
   background: #911e95 !important;
   color: white;
 }
-.item-list .svg {
-  visibility: hidden;
-  pointer-events: none;
-}
-.item-list:hover .svg {
-  visibility: visible;
-}
 .item-button.selected {
-  animation: expand 1s infinite;
+  animation: expand 1.5s infinite;
 }
 @keyframes expand {
   0% {
     transform: translate(-50%, -50%) scale(1);
   }
   50% {
-    transform: translate(-50%, -50%) scale(1.3);
+    transform: translate(-50%, -50%) scale(1.25);
   }
   100% {
     transform: translate(-50%, -50%) scale(1);
